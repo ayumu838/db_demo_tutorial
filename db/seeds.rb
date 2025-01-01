4c1203333e9799d2f5ea4e182d3937de87765bd5
ayumu.mochizuki <ayumu839.a@gmail.com>
2025-01-02 00:04:03 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,70 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+BASE_RECORD_NUM = 100_000
+BATCH_SIZE = 1000
+
+(BASE_RECORD_NUM/BATCH_SIZE).times do |i|
+  Company.transaction do
+    BATCH_SIZE.times do
+      Company.create(
+        name: Faker::Company.name,
+        address: Faker::Address.state + Faker::Address.city + Faker::Address.street_name + Faker::Address.state_abbr
+      )
+    end
+  end
+  puts("Company: #{i + 1}/#{BASE_RECORD_NUM/BATCH_SIZE} completed.")
+end
+
+Faker::Config.locale = :en
+
+(BASE_RECORD_NUM/BATCH_SIZE).times do |i|
+  Shop.transaction do
+    BATCH_SIZE.times do
+      Shop.create(
+        company_id: Company.first.id + Random.rand(BASE_RECORD_NUM),
+        name: Faker::Company.name,
+        description: 'dummy' * Random.rand(10)
+      )
+    end
+  end
+  puts("Shop: #{i + 1}/#{BASE_RECORD_NUM/BATCH_SIZE} completed.")
+end
+
+(BASE_RECORD_NUM/BATCH_SIZE).times do |i|
+  Work.transaction do
+    BATCH_SIZE.times do
+      Work.create(
+        shop_id: Shop.first.id + Random.rand(BASE_RECORD_NUM),
+        name: Faker::Company.name,
+        description: 'dummy' * Random.rand(10),
+        salary: 1000 + Random.rand(1000)
+      )
+    end
+  end
+  puts("Work: #{i + 1}/#{BASE_RECORD_NUM/BATCH_SIZE} completed.")
+end
+
+Faker::Config.locale = :ja
+
+((BASE_RECORD_NUM * 4)/BATCH_SIZE).times do |i|
+  Member.transaction do
+    (BATCH_SIZE).times do
+      Member.create(
+        name: Faker::Name.name,
+        email_address: Faker::Internet.email,
+        birthday: Faker::Date.between(from: '1960-01-01', to: '2003-12-31')
+      )
+    end
+    puts("Member: #{i + 1}/#{(BASE_RECORD_NUM * 4)/BATCH_SIZE} completed.")
+  end
+end
+
+((BASE_RECORD_NUM * 5)/BATCH_SIZE).times do |i|
+  Entry.transaction do
+    (BATCH_SIZE).times do
+      Entry.create(
+        work_id: Work.first.id + Random.rand(BASE_RECORD_NUM),
+        member_id: Member.first.id + Random.rand(BASE_RECORD_NUM *  4)
+      )
+    end
+    puts("Entry: #{i + 1}/#{(BASE_RECORD_NUM * 5)/BATCH_SIZE} completed.")
+  end
+end
